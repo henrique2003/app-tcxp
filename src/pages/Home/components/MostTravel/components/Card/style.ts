45be2687a9f9af4aa82f16c5966c27180486839e
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-06-01 18:42:58 -0300

    style: responsive cards of most visiteds tablets

--- src/pages/Home/components/MostTravel/components/Card/style.ts
+++ src/pages/Home/components/MostTravel/components/Card/style.ts
@@ -5,51 +5,60 @@ interface Props {
 }
 
 export const StyledCard = styled.div<Props>`
-  /* max-width: 350px; */
   position: absolute;
   top: 50%;
   left: 50%;
   height: 500px;
-  max-width: 350px;
-  background: url(${props => props.image});
+  width: 350px;
+  background: url(${(props) => props.image});
   background-size: cover;
   background-position: -50%, -50%;
   background-repeat: no-repeat;
   border-radius: 5px;
 
   &:nth-child(3) {
-    transform: translateX( -50%);
+    transform: translateX(-50%);
     z-index: 5;
 
     .content {
+      h5 {
+        font-size: 23px;
+      }
+
       p {
         font-size: 19px;
       }
     }
   }
 
-  &:nth-child(2), &:nth-child(4) {
+  &:nth-child(2),
+  &:nth-child(4) {
     height: 420px;
-    max-width: 300px;
+    width: 300px;
     margin-top: 50px;
     z-index: 4;
   }
 
   &:nth-child(2) {
-    transform: translateX( -135%);
+    transform: translateX(-135%);
   }
 
   &:nth-child(4) {
-    transform: translateX( 35%);
+    transform: translateX(35%);
   }
 
-  &:first-child, &:last-child {
+  &:first-child,
+  &:last-child {
     height: 365px;
-    max-width: 280px;
+    width: 280px;
     margin-top: 83px;
     z-index: 3;
 
     .content {
+      h5 {
+        font-size: 18px;
+      }
+
       p {
         font-size: 16px;
       }
@@ -79,5 +88,49 @@ export const StyledCard = styled.div<Props>`
       margin-top: 10px;
       padding-right: 35px;
     }
-  }  
+  }
+
+  /* hide first and last card */
+  @media (max-width: 1200px) {
+    &:first-child,
+    &:last-child {
+      display: none;
+    }
+  }
+
+  /* Tablets */
+  @media (max-width: 900px) {
+    height: 450px;
+    width: 300px;
+
+    &:nth-child(3) {
+      .content {
+        h5 {
+          font-size: 22px;
+        }
+
+        p {
+          font-size: 18px;
+        }
+      }
+    }
+
+    &:nth-child(2),
+    &:nth-child(4) {
+      height: 380px;
+      width: 250px;
+      margin-top: 44px;
+    }
+
+    .content {
+      h5 {
+        font-size: 19px;
+      }
+
+      p {
+        font-size: 16px;
+        margin-top: 8px;
+      }
+    }
+  }
 `
