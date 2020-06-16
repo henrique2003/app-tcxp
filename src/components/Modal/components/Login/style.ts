09ac9354b3e51df28bebfd8bf166966bb15101ca
Henrique De Melo Cristioglu <henrique.de.melo.cristioglu@gmail.com>
2020-06-16 10:57:52 -0300

    style: responsive login modal

--- src/components/Modal/components/Login/style.ts
+++ src/components/Modal/components/Login/style.ts
@@ -1,12 +1,33 @@
 import styled from 'styled-components'
 
 export const StyledLogin = styled.div`
-  padding-top: 5px;
-  padding-bottom: 100px;
+  height: 450px;
 
-  h3 {
-    text-transform: none !important;
+  header {
     margin-bottom: 35px;
+    margin-top: 100px !important;
+
+    h3 {
+      text-transform: none !important;
+    }
+
+    //Ruduce font size for Tablets
+    @media(max-width: 840px) {
+      margin-bottom: 30px;
+
+      h3 {
+        font-size: 29px;
+      }
+    }
+
+    //Ruduce font size for celphones
+    @media(max-width: 400px) {
+      margin-bottom: 25px;
+
+      h3 {
+        font-size: 27px;
+      }
+    }
   }
 
   p {
@@ -21,5 +42,45 @@ export const StyledLogin = styled.div`
   button[type="button"] {
     margin: 0;
     max-width: 100px;
+  }
+
+  //Celphones
+  @media(max-width: 700px) {
+    height: 400px;
+
+    header {
+      margin-top: 50px !important;
+
+      h3 {
+        font-size: 35px;
+      }
+    }
+  }
+
+  //Small celphones
+  @media(max-width: 470px) {
+    height: 370px;
+
+    header {
+      margin-top: 70px !important;
+
+      h3 {
+        font-size: 30px;
+      }
+    }
+  }
+
+  //Ruduce all lengths
+  @media(max-width: 380px) {
+    height: 330px;
+
+    header {
+      margin-top: 50px !important;
+      margin-bottom: 25px;
+      
+      h3 {
+        font-size: 25px;
+      }
+    }
   }
 `
