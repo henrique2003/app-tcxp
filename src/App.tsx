9bda3baa474c3042a2503ff9a956d29497d79ca3
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-08-17 10:01:32 -0300

    style: make page email confirmation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Switch } from 'react-router-dom'
+import { BrowserRouter, Switch, withRouter, RouteComponentProps } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { Header, Modal, Footer } from './components'
 import Routes from './routes'
@@ -8,15 +8,33 @@ import store from './store'
 import './global.css'
 
 const App: React.FC = () => {
+  // WithRouter Header
+  const WithHeader = withRouter(({ location }: RouteComponentProps): JSX.Element => {
+    if (location.pathname === '/' ||
+      location.pathname === '/sobre' ||
+      location.pathname === 'duvidas'
+    ) return <Header />
+    else return <></>
+  })
+
+  // WithRouter Footer
+  const WithFooter = withRouter(({ location }: RouteComponentProps): JSX.Element => {
+    if (location.pathname === '/' ||
+      location.pathname === '/sobre' ||
+      location.pathname === 'duvidas'
+    ) return <Footer />
+    else return <></>
+  })
+
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Header />
+        <WithHeader />
         <Modal />
         <Switch>
           <Routes />
         </Switch>
-        <Footer />
+        <WithFooter />
       </BrowserRouter>
     </Provider>
   )
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,11 @@
 import Home from './Home'
 import About from './About'
 import Questions from './Questions'
+import EmailConfirmation from './EmailConfirmation'
 
 export {
   Home,
   About,
-  Questions
+  Questions,
+  EmailConfirmation
 }
