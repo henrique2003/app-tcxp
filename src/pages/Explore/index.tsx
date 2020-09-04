7fc9c30a63eaaff40144b89aca02966e05329136
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-09-03 21:42:15 -0300

    style: make explore page

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { BsSearch } from 'react-icons/bs'
+import { BgExplore, PictureProfile } from '../../assets'
+import { Container } from '../../styles'
+import { DivSearch, Input, Phrase, StyledExplore, DivInput, TitlePersons, Grid } from './style'
+import PersonItem from './PersonItem'
 
 const Explore: React.FC = () => {
   return (
-    <div>
-
-    </div>
+    <StyledExplore>
+      <DivSearch url={BgExplore}>
+        <Container>
+          <Phrase>Fassa sua pesquisa e encontre um parceiro ou parceira para  a sua próxima viagem</Phrase>
+          <DivInput>
+            <Input type="text" placeholder="Procurando alguém ?"/>
+            <BsSearch />
+          </DivInput>
+        </Container>
+      </DivSearch>
+      <Container>
+        <TitlePersons>Pessoas que talvez você goste</TitlePersons>
+        <Grid>
+          <PersonItem image={PictureProfile}/>
+          <PersonItem image={PictureProfile}/>
+          <PersonItem image={PictureProfile}/>
+        </Grid>
+      </Container>
+    </StyledExplore>
   )
 }
 
