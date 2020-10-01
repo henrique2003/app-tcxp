6e18959893f31d9fd204273ba224500940fa2d13
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-10-01 10:28:59 -0300

    style: change style from about page

--- src/pages/About/components/Wrapper/index.tsx
+++ src/pages/About/components/Wrapper/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Container } from '../../../../styles'
-import { AboutWrapper } from '../../../../assets'
 import { StyledWrapper } from './style'
 
 const Wrapper: React.FC = () => {
   return (
-    <StyledWrapper background={AboutWrapper}>
-      <Container>
-        <h4>Você decide o que fazer!</h4>
-        <p>Você pode escolher a qualquer hora e em qualquer lugar sem  depender de ninguém para te aconpanhar nesta jornada, Você pode escolher a qualquer hora e em qualquer lugar sem  depender de ninguém para te aconpanhar nesta jornada.</p>
-      </Container>
-    </StyledWrapper>
+    <>
+      <StyledWrapper>
+        <Container>
+          <h4>Você decide o que fazer!</h4>
+          <p>Você pode escolher a qualquer hora e em qualquer lugar sem  depender de ninguém para te aconpanhar nesta jornada, Você pode escolher a qualquer hora e em qualquer lugar sem  depender de ninguém para te aconpanhar nesta jornada.</p>
+        </Container>
+      </StyledWrapper>
+    </>
   )
 }
 
