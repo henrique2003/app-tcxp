0ff31b0fe9fbadeaabcc977b2eff795d6aed710a
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-10-11 18:22:34 -0300

    feat: make show my groups in side bar

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,11 @@
 import { isValidFields } from './valid-fields'
 import setToken from './set-token'
 import titleize from './titelize'
+import editDate from './edit-date'
 
 export {
   isValidFields,
   setToken,
-  titleize
+  titleize,
+  editDate
 }
